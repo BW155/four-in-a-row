057c1a1df18fea122ba7b7e0fc3731c2dc1b24c9
DerUberCatman <b.wisman155@gmail.com>
2018-09-13 20:00:59 +0200

    fixed check patterns

--- src/winner_calc.rs
+++ src/winner_calc.rs
@@ -1,32 +1,32 @@
 use objects::Position;
 use BOARD_SIZE;
 
-pub const UP: [(i64, i64); 4] = [(0, 0), (0, 1), (0, 2), (0, 3)];
-pub const RIGHT: [(i64, i64); 4] = [(0, 0), (1, 0), (2, 0), (3, 0)];
-pub const UP_RIGHT: [(i64, i64); 4] = [(0, 0), (-1, 1), (-2, 2), (-3, 3)];
-pub const UP_LEFT: [(i64, i64); 4] = [(0, 0), (1, 1), (2, 2), (3, 3)];
+pub const UP: [(i64, i64); (BOARD_SIZE / 2) as usize] = [(0, 0), (0, 1), (0, 2), (0, 3)];
+pub const RIGHT: [(i64, i64); (BOARD_SIZE / 2) as usize] = [(0, 0), (1, 0), (2, 0), (3, 0)];
+pub const UP_RIGHT: [(i64, i64); (BOARD_SIZE / 2) as usize] = [(0, 0), (-1, 1), (-2, 2), (-3, 3)];
+pub const UP_LEFT: [(i64, i64); (BOARD_SIZE / 2) as usize] = [(0, 0), (1, 1), (2, 2), (3, 3)];
 
 pub fn calculate_winner(positions: &Vec<Position>) -> i64 {
     for p in positions.clone() {
         let x = p.x;
         let y = p.y;
         if y < (BOARD_SIZE - 3) {
-            if direction_taken(UP, &x, &y, positions.clone()) {
+            if direction_taken(UP, &x, &y, &positions) {
                 return p.player;
             }
             if x > 2 {
-                if direction_taken(UP_RIGHT, &x, &y,  positions.clone()) {
+                if direction_taken(UP_RIGHT, &x, &y,  &positions) {
                     return p.player;
                 }
             }
             if x < (BOARD_SIZE - 3)  {
-                if direction_taken(UP_LEFT, &x, &y, positions.clone()) {
+                if direction_taken(UP_LEFT, &x, &y, &positions) {
                     return p.player;
                 }
             }
         }
         if x < (BOARD_SIZE - 3) {
-            if direction_taken(RIGHT, &x, &y,  positions.clone()) {
+            if direction_taken(RIGHT, &x, &y,  &positions) {
                 return p.player;
             } 
         } 
